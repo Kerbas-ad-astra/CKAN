eb6e314bc8e597a88913bc6b207c3f9aca469530
Dwayne Bent <dbb@dbb.io>
2015-07-16 13:50:55 -0400

    Add PropertySortTransformer

--- Netkan/Transformers/NetkanTransformer.cs
+++ Netkan/Transformers/NetkanTransformer.cs
@@ -37,7 +37,8 @@ namespace CKAN.NetKAN.Transformers
                 new DownloadSizeTransformer(http, fileService),
                 new GeneratedByTransformer(),
                 new OptimusPrimeTransformer(),
-                new StripNetkanMetadataTransformer()
+                new StripNetkanMetadataTransformer(),
+                new PropertySortTransformer()
             };
         }
 
